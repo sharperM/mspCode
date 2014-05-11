22d630479cc3d87f3d12725855b4dfcf423db642
msharper <soldier-fox@hotmail.com>
2014-05-12 00:00:56 +0800

    modified:   MFCApplication1.sln 	modified:   Win32Project1/Win32Project1.cpp 	modified:   Win32Project1/Win32Project1.vcxproj 	modified:   Win32Project1/Win32Project1.vcxproj.filters 	modified:   Win32Project1/stdafx.h

--- Win32Project1/stdafx.h
+++ Win32Project1/stdafx.h
@@ -2,6 +2,8 @@
 // 或是经常使用但不常更改的
 // 特定于项目的包含文件
 //
+#if !defined(AFX_STDAFX_H__08E7CDC6_7F58_11D2_8CF1_00A0C9441E20__INCLUDED_)
+#define AFX_STDAFX_H__08E7CDC6_7F58_11D2_8CF1_00A0C9441E20__INCLUDED_
 
 #pragma once
 
@@ -10,7 +12,6 @@
 #define WIN32_LEAN_AND_MEAN             //  从 Windows 头文件中排除极少使用的信息
 // Windows 头文件: 
 #include <windows.h>
-#include <Windowsx.h>
 // C 运行时头文件
 #include <stdlib.h>
 #include <malloc.h>
@@ -18,5 +19,9 @@
 #include <tchar.h>
 #define WM_GETIPMSG (WM_USER + 1)
 #define WM_SOCKET_NOTIFY (WM_USER + 2)
+#include <dshow.h>
+#include <assert.h>
+
 
 // TODO:  在此处引用程序需要的其他头文件
+#endif // !defined(AFX_STDAFX_H__08E7CDC6_7F58_11D2_8CF1_00A0C9441E20__INCLUDED)
