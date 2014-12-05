6c997647a9b95e00cad03e4352bc88b0c7aa189e
sharper <mshaopei@gmail.com>
2014-12-05 15:56:15 +0800

    Changes to be committed: 	new file:   LuaUseClass.lua 	new file:   class.lua
     Changes not staged for commit:
    	modified:   ProgramingWithLua.cpp
    	modified:   sp.lua

---
+++ ProgramingWithLua/LuaUseClass.lua
@@ -0,0 +1,5 @@
+c = CTest()
+print("c.m_x ==> " .. c.m_x);
+c.m_x = 1
+print("c.m_x ==> " .. c.m_x);
+print("c.Add(4, 5) ==> " .. c:Add(4, 5));
---
+++ ProgramingWithLua/class.lua
@@ -0,0 +1,6 @@
+print ("class.lua__ start")
+
+foo = l_Foo_constructor("msp")
+
+
+print ("class.lua__ end")
